bd5dae9e89cd35ca304998ddb5dc5a04ae74ec6a
Udgeet <bhatt8udgeet@gmail.com>
2024-04-18 21:27:13 +0530

    Add Body Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
-import HomeHeader from "@/components/HomeHeader"
-
+import HomeHeader from '@/components/HomeHeader';
+import HomeSearch from '@/components/HomeSearch';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-    <HomeHeader />
+      <HomeHeader />
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+          src="/images/google.png"
+          alt='Google Logo'
+          width={200}
+          height={100}
+          priority
+          style={{ width: 'auto' }}
+        />
+        <HomeSearch />
+      </div>
     </>
-    
-  )
+  );
 }
+
+
+ // src="/images/google.png"
